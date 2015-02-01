2608a79477ab690b930b5d228f9f051024c65ff8
Gabe Rubio <gtrubio192@gmail.com>
2015-01-31 21:09:05 -0600

    Added alot of cool shit. Left and right align depending on who user is. Sign in page now mandatory to enter chat room. Working on getting away status to post on your behalf. Refresh works tho

--- js/main.js
+++ js/main.js
@@ -1,21 +1,68 @@
    
 $(document).ready(function() {
 	// var name = prompt("Enter your user name: ");
+	// enter a valid name, hide all chat room
+
+	var name = '';
+	$('#chat-room').hide();
+	$('#login').on('click', 
+		function(){
+			name = $('#login-text').val();
+			if(name == '')
+			{
+				alert("Please enter user name");
+			}
+			else{
+				$('#chat-room').show();
+				$('#sign-in').hide();
+			}
+
+	});
 
 	$('#submit').on('click', onSearchButtonClick);
-	//testing call
-	// $('table').on('.entry tr', 'click', onRowClick);
+	// refresh listener
+	$('#refresh').on('click', 
+			function() { 
+				// alert("clicked refresh");
+				$('#chat-room').show();
+				$('#sign-in').hide();
+				location.reload(); 
+				
+			});
+
+	// status listener
+	$('a .status').on('click', 
+		function() {
+			var status = document.getElementById();
+			alert("status: " + status);
+			if(status == 'Away')
+			{
+				alert("Away chosen");
+				$.post(
+					'https://kate-gabe-chat.herokuapp.com/chats',
+					{
+						chat: {
+							message: "Currently Away",
+							name: name,
+							like: '',
+							photo: ''
+						}
+					
+					},
+					function(message) {
+						console.log("Posting: " + message);
+						// $('textarea').html('');
+						render(message);
+					},
+					'json'
+				);
+			}
+
+	});
 
 	function onSearchButtonClick() {
 		// 1. Input value
-		console.log("Search: " + $('#chat-box').val());
-		//$('.submit').val();
-		// $('#results').html($('#search-box').val());
-		// var now = timeStamp();
-
-		// clear text box
-		var now = timeStamp();
-		// var name = prompt("Enter your user name: ");
+		console.log("Message: " + $('#chat-box').val());
 
 		$.post(
 			// 'http://tiny-pizza-server.herokuapp.com/collections/austintime',
@@ -23,16 +70,14 @@ $(document).ready(function() {
 			{
 				chat: {
 					message: $('#chat-box').val(),
-					// name: name,
-					name: "CoolDude",
-					// created_at: now,
+					name: name,
 					like: '',
 					photo: ''
 				}
 			
 			},
 			function(message) {
-				console.log(message);
+				console.log("Posting: " + message);
 				// $('textarea').html('');
 				render(message);
 			},
@@ -52,58 +97,31 @@ $(document).ready(function() {
 		);
 	};
 
-	function timeStamp(dateString) {
-	// Create a date object with the current time
-    var now = new Date(dateString);
-	 
-	// Create an array with the current month, day and time
-	  var date = [ now.getMonth() + 1, now.getDate(), now.getFullYear() ];
-	 
-	// Create an array with the current hour, minute and second
-	  var time = [ now.getHours(), now.getMinutes(), now.getSeconds() ];
-	 
-	// Determine AM or PM suffix based on the hour
-	  var suffix = ( time[0] < 12 ) ? "AM" : "PM";
-	 
-	// Convert hour from military time
-	  time[0] = ( time[0] < 12 ) ? time[0] : time[0] - 12;
-	 
-	// If hour is 0, set it to 12
-	  time[0] = time[0] || 12;
-	 
-	// If seconds and minutes are less than 10, add a zero
-	  for ( var i = 1; i < 3; i++ ) {
-	    if ( time[i] < 10 ) {
-	      time[i] = "0" + time[i];
-	    }
-	  }
-	 
-	// Return the formatted string
-	  return date.join("/") + " " + time.join(":") + " " + suffix;
-	}
-
 	var render = function(messages) {
-		console.log(messages);
-		var messageRow = _.template('<li><div class="chat-body clearfix"><div class="header"><strong class="primary-font"><%= name %></strong><small class="pull-right"><span class="glyphicon glyphicon-heart"> <%= like %> </span></small></div><p><%= message %></p></div><div><%= time_stamp %></div></li>');
+		// console.log(messages);
+		var messageRowMe = _.template('<li class="right clearfix"><span class="chat-img pull-right"><img src="http://placehold.it/50/FA6F57/fff&amp;text=ME" alt="User Avatar" class="img-circle"></span><div class="chat-body clearfix"><div class="header"><small><span id="likes" class="glyphicon glyphicon-heart"> <%= like %> </span></small><strong class="pull-right primary-font"><%= name %></strong></div><p><%= message %></p><div><%= time_stamp %></div></div></li>');
+		var messageRowYou = _.template('<li class="left clearfix"><span class="chat-img pull-left"><img src="http://placehold.it/50/55C1E7/fff&amp;text=YOU" alt="User Avatar" class="img-circle"></span><div class="chat-body clearfix"><div class="header"><small><span id="likes" class="glyphicon glyphicon-heart"> <%= like %> </span></small><strong class="pull-left primary-font"><%= name %></strong></div><p><%= message %></p><div><%= time_stamp %></div></div></li>');
 		// console.log(messages);
 		$('#message-board').html('');
 		for(var i=0; i<messages.length; i++) {
 			// if(messages[i].message && messages[i].name && messages[i].created_at) {
-				$('.chat').prepend(messageRow(messages[i]));
-			// }
-			// else{
-				// console.log("Failed");
-			// }
+			if(messages[i].name == name){
+				console.log("Name at i: " + messages[i].name + "     Current user: " + name);
+				$('.chat').prepend(messageRowMe(messages[i]));
+			}
+			else{
+				$('.chat').prepend(messageRowYou(messages[i]));
+			}
+
 		}
 	};
+
 
 	
 	// var now = timeStamp();
 	// var firstPass = 1;
 	getMessages();
 	setInterval( getMessages, 3000);
-
-
 
 });
 
